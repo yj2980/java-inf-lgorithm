657a0d2c2cb91038675d04cd7a5a827ab95c33d4
yj2980 <gum0945@naver.com>
2022-12-15 20:48:24 +0900

    feat : add function that replace upper and lowercase

---
+++ java-algorithm-problemSolving/src/main/java/problem2/ChangeCase.java
@@ -0,0 +1,30 @@
+package problem2;
+
+import java.util.Scanner;
+
+public class ChangeCase {
+	public static void main(String[] args){
+		Scanner in= new Scanner(System.in);
+		String word = in.next();
+
+		System.out.println(new ChangeCase().solution(word));
+	}
+
+	private String solution(String word) {
+		StringBuilder result = new StringBuilder();
+
+		for (char c : word.toCharArray()) {
+			result.append(isLowerOrUpperCase(c));
+		}
+
+		return result.toString();
+	}
+
+	private static char isLowerOrUpperCase(char separator) {
+		if (Character.isUpperCase(separator)) {
+			return (char)(separator + 32);
+		}
+
+		return (char)(separator - 32);
+	}
+}
