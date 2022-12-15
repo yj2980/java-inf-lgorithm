e468eefd11d0b84cdd5cc96ad7e05299214fbaed
yj2980 <gum0945@naver.com>
2022-12-15 20:38:02 +0900

    feat : add function to find characters by capitalizing them all

--- java-algorithm-problemSolving/src/main/java/problem1/FindTextOtherSolution.java
+++ java-algorithm-problemSolving/src/main/java/problem1/FindTextOtherSolution.java
@@ -1,2 +1,34 @@
-package problem1;public class FindTextOtherSolution {
+package problem1;
+
+import java.util.Scanner;
+
+public class FindTextOtherSolution {
+	public static void main(String[] args){
+		String word = readWord();
+		char separator = readWord().charAt(0);
+
+		int answer = solution(word.toUpperCase(), Character.toUpperCase(separator));
+
+		System.out.println(answer);
+	}
+
+	public static int solution(String word, char separator) {
+		int cnt = 0;
+
+		for (char c : word.toCharArray()) {
+			if (c == separator) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+
+
+	private static String readWord() {
+		Scanner in = new Scanner(System.in);
+
+		return in.next();
+	}
+
 }
