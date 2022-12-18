d8f8c9ff10ddae46debcf8764a585d57c3ceea5c
yj2980 <gum0945@naver.com>
2022-12-18 23:05:51 +0900

    feat : add function to remove duplicate characters

--- src/main/java/problem6/RemoveDuplicateCharacters.java
+++ src/main/java/problem6/RemoveDuplicateCharacters.java
@@ -1,2 +1,34 @@
-package problem6;public class RemoveDuplicateCharacters {
+package problem6;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicateCharacters {
+	static RemoveDuplicateCharacters main = new RemoveDuplicateCharacters();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		List<Character> characters = main.convertStringToList(sc.next());
+		main.remove(characters);
+
+		characters.forEach(v -> System.out.print(v));
+	}
+
+	private void remove(List<Character> characters) {
+		for (int i = 0; i < characters.size(); i++) {
+			if (i != characters.indexOf(characters.get(i))) {
+				characters.remove(i);
+				i--;
+			}
+		}
+	}
+
+	private List<Character> convertStringToList(String word) {
+
+		return word.chars()
+				.mapToObj(e -> (char)e)
+				.collect(Collectors.toList());
+	}
 }
