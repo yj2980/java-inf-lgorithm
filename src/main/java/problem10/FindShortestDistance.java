0f56c65aeeacfafd13f0e36603463a5b0d7245e5
yj2980 <gum0945@naver.com>
2022-12-19 21:59:47 +0900

    feat : add function to find shortest distance from specific character

--- src/main/java/problem10/FindShortestDistance.java
+++ src/main/java/problem10/FindShortestDistance.java
@@ -1,2 +1,62 @@
-package problem10;public class FindShortestDistance {
+package problem10;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindShortestDistance {
+	static FindShortestDistance main = new FindShortestDistance();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String word = sc.next();
+		String sep = sc.next();
+
+		List<Integer> location = main.locateSpecificCharacter(word, sep.charAt(0));
+		List<Integer> result = main.findShortestDistanceFromCharacter(word, location);
+
+		result.forEach(v -> System.out.print(v + " "));
+
+	}
+
+	private List<Integer> locateSpecificCharacter(String word, char sep) {
+		int index = 0;
+		List<Integer> location = new ArrayList<>();
+
+		for (char letter : word.toCharArray()) {
+			if (letter == sep) {
+				location.add(index);
+			}
+			index++;
+		}
+
+		return location;
+	}
+
+	private List<Integer> findDistanceFromSpecificCharacter(int currentLocation, List<Integer> location) {
+		List<Integer> distance = new ArrayList<>();
+
+		for (int d = 0; d < location.size(); d++) {
+			int specificLocation = location.get(d);
+			int max = Math.max(currentLocation, specificLocation);
+			int min = Math.min(currentLocation, specificLocation);
+
+			distance.add(max - min);
+		}
+
+		return distance;
+	}
+
+	private List<Integer> findShortestDistanceFromCharacter(String word, List<Integer> location) {
+		List<Integer> result = new ArrayList<>();
+
+		for (int i = 0; i < word.length(); i++) {
+			List<Integer> distance = findDistanceFromSpecificCharacter(i, location);
+			result.add(Collections.min(distance));
+		}
+
+		return result;
+	}
 }
