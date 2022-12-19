6697dc7023ee175d3a481fc49753d065e0cf598f
yj2980 <gum0945@naver.com>
2022-12-19 21:58:55 +0900

    feat : add function to exclude characters except numbers

--- src/main/java/problem9/ExcludeCharactersExceptNumbers.java
+++ src/main/java/problem9/ExcludeCharactersExceptNumbers.java
@@ -1,2 +1,25 @@
-package problem9;public class ExcludeCharactersExceptNumbers {
+package problem9;
+
+import java.util.Scanner;
+
+public class ExcludeCharactersExceptNumbers {
+	static ExcludeCharactersExceptNumbers main = new ExcludeCharactersExceptNumbers();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(main.excludeCharacters(sc.next()));
+	}
+
+	private int excludeCharacters(String word) {
+		String answer = "";
+
+		for (char letter : word.toCharArray()) {
+			if (letter >= '0' && letter <= '9') {
+				answer += letter;
+			}
+		}
+
+		return Integer.parseInt(answer);
+	}
 }
