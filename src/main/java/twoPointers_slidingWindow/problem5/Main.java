edf26e08fa3f3a0509d2fc2b5520638cba2a2531
yj2980 <gum0945@naver.com>
2023-01-11 18:14:55 +0900

    feat : solve the sliding window algorithm

--- src/main/java/twoPointers_slidingWindow/problem5/Main.java
+++ src/main/java/twoPointers_slidingWindow/problem5/Main.java
@@ -1,2 +1,39 @@
-package twoPointers_slidingWindow.problem5;public class Main {
+package twoPointers_slidingWindow.problem5;
+
+import java.util.Scanner;
+
+public class Main {
+	static Main main = new Main();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.next());
+		int answer = main.solution(n);
+
+		System.out.println(answer);
+	}
+
+	public int solution(int n) {
+		int sum = 0;
+		int count = 0;
+		int k = 1;
+
+		 for (int i = 0; i <= n / 2 + 1; i++) {
+			sum += i;
+
+			if (sum == n) {
+				count++;
+			}
+
+			 while (sum >= n) {
+				 sum -= k++;
+				 if (sum == n) {
+					 count++;
+				 }
+			 }
+		 }
+
+		 return count;
+	}
 }
