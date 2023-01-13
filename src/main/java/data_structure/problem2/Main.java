87d6ae3cd40b3ed251d0301a3c34bc03e6616b6a
yj2980 <gum0945@naver.com>
2023-01-13 23:24:21 +0900

    feat : solve the stack algorithm problem

--- src/main/java/data_structure/problem2/Main.java
+++ src/main/java/data_structure/problem2/Main.java
@@ -1,2 +1,50 @@
-package data_structure.problem2;public class Main {
+package data_structure.problem2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	static Main main = new Main();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		List<String> letters = main.convertStringToList(sc.next());
+		main.printResult(main.removeBracket(letters));
+	}
+
+	private List<String> convertStringToList(String s) {
+		List<String> letters = new ArrayList<>();
+
+		for (char c : s.toCharArray()) {
+			letters.add(String.valueOf(c));
+		}
+
+		return letters;
+	}
+
+	private Stack<String> removeBracket(List<String> letters) {
+		Stack<String> word = new Stack<>();
+
+		for (String alpha : letters) {
+			if (alpha.equals("(")) {
+				word.push(alpha);
+			}
+			if (alpha.equals(")")) {
+				word.pop();
+				continue;
+			}
+			if (!word.contains("(")) {
+				word.push(alpha);
+			}
+		}
+
+		return word;
+	}
+
+	private void printResult(Stack<String> word) {
+		word.forEach(v -> System.out.print(v));
+	}
 }
